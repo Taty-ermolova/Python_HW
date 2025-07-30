cb4f488b35ed6fde614bc449c30ff4d2df756cc2
Taty-ermolova <taty-ermolova@yandex.ru>
2025-07-30 15:12:11 +0300

    lesson1

---
+++ lesson1/lesson1_task_5.py
@@ -0,0 +1,14 @@
+def print_numer(num):
+    print(num, end = '')
+
+print_numer('8')
+print_numer('8')
+print_numer('0')
+print_numer('0')
+print_numer('5')
+print_numer('5')
+print_numer('5')
+print_numer('3')
+print_numer('5')
+print_numer('3')
+print_numer('5')
